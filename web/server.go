2d4caa520f92daa9b39d13dff1309f13f9ab27e0
Rafael Chargel <rchargel@chariotsolutions.com>
2014-03-13 15:57:50 -0400

    push

--- web/server.go
+++ web/server.go
@@ -2,28 +2,28 @@ package web
 
 import (
 	"fmt"
-	"github.com/rchargel/goblog/conf"
 	"net/http"
 )
 
 type Server struct {
-	config conf.Config
+	config *Config
 }
 
 func Root(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "TESTING")
+	w.Header().Set("Content-type", "application/javascript; charset=utf-8")
+	fmt.Fprintf(w, `{"test":"good"}`)
 }
 
 func NewServer() *Server {
 	var server *Server
 	server = new(Server)
-	server.config = conf.NewConfig()
+	server.config = NewConfig()
 	return server
 }
 
 func (s *Server) Start() {
 	http.HandleFunc("/", Root)
-	fmt.Println("Listening on port:", s.config.Port)
+	fmt.Println("Listening on port:", s.config.port)
 
-	http.ListenAndServe(fmt.Sprintf(":%v", s.config.Port), nil)
+	http.ListenAndServe(fmt.Sprintf(":%v", s.config.port), nil)
 }
