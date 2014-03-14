c9acfae1fa73ef292f8e1eb9a1e24f7d18da770a
Rafael Chargel <rchargel@chariotsolutions.com>
2014-03-14 13:15:21 -0400

    Adding bookmarks

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,6 +1,17 @@
 package controllers
 
-import "github.com/robfig/revel"
+import (
+	"encoding/json"
+	"github.com/robfig/revel"
+	"io"
+	"os"
+)
+
+type Bookmark struct {
+	Link     string
+	LinkText string
+	Message  string
+}
 
 type App struct {
 	*revel.Controller
@@ -8,4 +19,25 @@ type App struct {
 
 func (c App) Index() revel.Result {
 	return c.Render()
+}
+
+func (c App) Bookmarks() revel.Result {
+	links := make([]Bookmark, 0, 10)
+	file, err := os.Open("./public/resources/bookmarks.json")
+	defer file.Close()
+	if err == nil {
+		decoder := json.NewDecoder(file)
+		for {
+			if err := decoder.Decode(&links); err == nil {
+				// links will be set
+			} else if err == io.EOF {
+				break
+			} else {
+				panic(err)
+			}
+		}
+	} else {
+		panic(err)
+	}
+	return c.Render(links)
 }
