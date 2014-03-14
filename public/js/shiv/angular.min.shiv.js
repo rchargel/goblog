0dfc08a11ad05918113a19cca49b48f76833b0b9
Rafael Chargel <rchargel@chariotsolutions.com>
2014-03-14 11:02:02 -0400

    updating

---
+++ public/js/shiv/angular.min.shiv.js
@@ -0,0 +1,1 @@
+document.createElement('ng-include');document.createElement('ng-pluralize');document.createElement('ng-view');document.createElement('ng:include');document.createElement('ng:pluralize');document.createElement('ng:view');
