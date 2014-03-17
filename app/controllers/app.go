8357165df93899de94f46118765f7ee88892462a
Rafael Chargel <rchargel@chariotsolutions.com>
2014-03-17 18:26:45 -0400

    Adding robots file and link to PDF

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -7,10 +7,20 @@ import (
 	"os"
 )
 
+type Robot string
+
 type Bookmark struct {
 	Link     string
 	LinkText string
 	Message  string
+}
+
+func (r Robot) Apply(req *revel.Request, resp *revel.Response) {
+	resp.Out.Header().Set("Content-Type", "text/plain")
+	resp.Out.Write([]byte("User-Agent: *\n"))
+	resp.Out.Write([]byte("Allow: "))
+	resp.Out.Write([]byte(r))
+	resp.Out.Write([]byte("\n"))
 }
 
 type App struct {
@@ -19,6 +29,10 @@ type App struct {
 
 func (c App) Index() revel.Result {
 	return c.Render()
+}
+
+func (c App) Robots() revel.Result {
+	return Robot("/")
 }
 
 func (c App) Bookmarks() revel.Result {
