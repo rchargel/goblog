1a960046f75cc08ead53297daa759cea63f2d1a6
Rafael Chargel <rchargel@zcarioca.net>
2015-08-07 14:11:12 -0400

    Converting blogs to use YAML

--- app/domain/blogs.go
+++ app/domain/blogs.go
@@ -2,16 +2,18 @@ package domain
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"os"
 	"regexp"
 	"sort"
 	"strings"
 	"time"
+
+	"gopkg.in/yaml.v2"
 )
 
+// NewBlogList creates the initialized bloglist
 func NewBlogList() *BlogList {
 	blog := &BlogList{}
 	blog.init()
@@ -19,10 +21,12 @@ func NewBlogList() *BlogList {
 	return blog
 }
 
+// Date the blog posted date
 type Date struct {
 	Time time.Time
 }
 
+// BlogItem a single blog entry
 type BlogItem struct {
 	Name     string
 	Date     Date
@@ -34,14 +38,18 @@ type BlogItem struct {
 	Index    map[string]int
 }
 
+// BlogItemByDate a sorted list of blog items.
 type BlogItemByDate []BlogItem
 
+// BlogItemFilterActive a filtered list of blog items.
 type BlogItemFilterActive []BlogItem
 
+// BlogList the returned bloglist.
 type BlogList struct {
 	items []BlogItem
 }
 
+// UnmarshalJSON reads the bytes into a JSON formatted string.
 func (d *Date) UnmarshalJSON(s []byte) error {
 	datetime := string(s)[0:len(s)-1] + "T00:00:00Z\""
 	return d.Time.UnmarshalJSON([]byte(datetime))
@@ -51,6 +59,7 @@ func (l BlogItemByDate) Len() int           { return len(l) }
 func (l BlogItemByDate) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
 func (l BlogItemByDate) Less(i, j int) bool { return l[i].Date.Time.After(l[j].Date.Time) }
 
+// Filter filters blogitems.
 func (l BlogItemFilterActive) Filter() []BlogItem {
 	newList := make([]BlogItem, 0, len(l))
 	for _, item := range []BlogItem(l) {
@@ -61,6 +70,7 @@ func (l BlogItemFilterActive) Filter() []BlogItem {
 	return newList
 }
 
+// GetContent gets the blog content
 func (b *BlogItem) GetContent() string {
 	buffer := &bytes.Buffer{}
 	file, err := os.Open("./public/resources/blog/" + b.File)
@@ -68,9 +78,8 @@ func (b *BlogItem) GetContent() string {
 	if err == nil {
 		buffer.ReadFrom(file)
 		return buffer.String()
-	} else {
-		return ""
 	}
+	return ""
 }
 
 func indexBlogItem(b BlogItem) BlogItem {
@@ -102,18 +111,31 @@ func indexBlogItem(b BlogItem) BlogItem {
 func (c *BlogList) init() {
 	if len(c.items) == 0 {
 		fmt.Println("INITIALIZE BLOG")
-		blogs := make([]BlogItem, 0, 15)
-		file, err := os.Open("./public/resources/blogs.json")
+		var blogs []BlogItem
+		file, err := os.Open("./public/resources/blogs.yml")
 		defer file.Close()
 		if err == nil {
-			decoder := json.NewDecoder(file)
-			for {
-				if err := decoder.Decode(&blogs); err == nil {
-					// resume will get set
-				} else if err == io.EOF {
-					break
-				} else {
-					panic(err)
+			data, err := ioutil.ReadAll(file)
+			if err == nil {
+				m := make(map[string]interface{})
+				yaml.Unmarshal(data, &m)
+				if list, ok := m["blogs"].([]interface{}); ok {
+					blogs = make([]BlogItem, len(list), len(list))
+					for i, item := range list {
+						if b, ok := item.(map[interface{}]interface{}); ok {
+							dateStr := b["date"].(string)
+							t, _ := time.Parse("2006-01-02", dateStr)
+							blogs[i] = BlogItem{
+								Name:     b["name"].(string),
+								Date:     Date{Time: t},
+								Abstract: b["abstract"].(string),
+								Slug:     b["slug"].(string),
+								Tags:     b["tags"].(string),
+								Active:   b["active"].(bool),
+								File:     fmt.Sprintf("%v.html", b["slug"]),
+							}
+						}
+					}
 				}
 			}
 		} else {
@@ -124,6 +146,7 @@ func (c *BlogList) init() {
 	}
 }
 
+// Index used by the controller to output the index.
 func (c *BlogList) Index() {
 	for i, item := range c.items {
 		if item.Active {
@@ -132,11 +155,13 @@ func (c *BlogList) Index() {
 	}
 }
 
+// GetFilteredList gets a filtered list of blogitems.
 func (c *BlogList) GetFilteredList() []BlogItem {
 	items := c.items
 	return BlogItemFilterActive(items).Filter()
 }
 
+// GetItem gets a single blog item.
 func (c *BlogList) GetItem(slug string) (*BlogItem, string, string) {
 	for i, item := range c.items {
 		if item.Slug == slug {
@@ -152,7 +177,7 @@ func (c *BlogList) GetItem(slug string) (*BlogItem, string, string) {
 						next = c.items[n].Slug
 						break
 					}
-					n -= 1
+					n--
 				}
 			}
 			if i < (len(c.items) - 1) {
@@ -166,7 +191,7 @@ func (c *BlogList) GetItem(slug string) (*BlogItem, string, string) {
 						prev = c.items[p].Slug
 						break
 					}
-					p += 1
+					p++
 				}
 			}
 			return &item, prev, next
